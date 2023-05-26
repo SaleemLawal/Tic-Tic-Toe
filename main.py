db8e31e3dcc1e2f8bcfa9cceac8de6c821681d1f
SaleemLawal <slawal1@umbc.edu>
2023-05-26 18:11:23 -0400

    Update Window

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 import pygame as pg
-from random import randint
+from random import randint  
+
+
+def initialize_screen():
+    screen.fill("white")
+    pg.display.flip()
+    
+if __name__ == '__main__':
+    game_play = True
+    pg.init()
+    screen = pg.display.set_mode(size=(700,700))
+    
+    while game_play:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                game_play = False
+        initialize_screen()        
+         
+    pg.quit()    
+        
 
